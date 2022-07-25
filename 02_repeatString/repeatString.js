832fe34f8641b0c2f1542fae47650878ad4fa33e
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-07-25 22:57:50 +0300

    Completed 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (count < 0) {
+        return "ERROR"
+    }
+    return str.repeat(count);
 };
 
 // Do not edit below this line
