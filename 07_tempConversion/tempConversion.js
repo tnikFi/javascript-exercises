a441d37eec2e9c3dc3a79f327ca88677e8523e0f
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-07-25 23:34:18 +0300

    Completed 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return parseFloat(((temp-32)/1.8).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return parseFloat(((temp*1.8)+32).toFixed(1));
 };
 
 // Do not edit below this line
