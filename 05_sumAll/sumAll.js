4b55a82911f71195877ba95b824976e2793705ed
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-07-25 23:19:09 +0300

    Completed 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (typeof(start) != "number" || typeof(end) != "number") {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    if (start <= end) {
+        for (let i = start; i < end+1; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = end; i < start+1; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
