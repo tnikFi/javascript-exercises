769f73e6adf4ac21fb9d40b4a3e9ba12b1cfeb73
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-07-29 20:56:03 +0300

    Completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((prev, curr) => {
+        let prevAge = prev.yearOfDeath ? prev.yearOfDeath-prev.yearOfBirth : new Date().getFullYear() - prev.yearOfBirth;
+        let currAge = curr.yearOfDeath ? curr.yearOfDeath-curr.yearOfBirth : new Date().getFullYear() - curr.yearOfBirth;
+        return prevAge > currAge ? prev : curr;
+    });
 };
 
 // Do not edit below this line
