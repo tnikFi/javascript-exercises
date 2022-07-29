38a49e9039a70260ce242163d95509a7bf13b55a
tnikFi <95296580+tnikFi@users.noreply.github.com>
2022-07-29 20:29:58 +0300

    Completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let chars = string.toLowerCase().split("").filter(char => /^[a-zA-Z0-9]$/.test(char));
+    let stripped = chars.join("");
+    let reversed = chars.reverse().join("");
+    return stripped == reversed;
 };
 
 // Do not edit below this line
